be7b19904ceb16231183a25e7cd3afc36cb972b1
Marc-Andre Ferland <madrang@gmail.com>
2022-10-24 02:01:40 -0400

    Added header with license.

--- buttons.plugin.js
+++ buttons.plugin.js
@@ -1,4 +1,21 @@
-// contributed by @madrang
+/* Madrang's SD-UI Plugin.js
+ *        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2022 Marc-Andre Ferland <madrang@gmail.com>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ * @link http://www.wtfpl.net/
+ * @source https://raw.githubusercontent.com/madrang/sd-ui-plugins/master/buttons.plugin.js
+ */
 (function() { "use strict"
     const GITHUB_PAGE = "https://github.com/madrang/sd-ui-plugins"
     const VERSION = "0.2.3.3";
