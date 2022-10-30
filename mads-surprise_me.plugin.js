11b811630f50c496169abcf9309fa615c759e7d9
Marc-Andre Ferland <madrang@gmail.com>
2022-10-29 20:51:12 -0400

    Bumped version after applying bug fixes.

--- mads-surprise_me.plugin.js
+++ mads-surprise_me.plugin.js
@@ -18,7 +18,7 @@
  */
 (function() { "use strict"
     const GITHUB_PAGE = "https://github.com/madrang/sd-ui-plugins"
-    const VERSION = "2.3.6.1";
+    const VERSION = "2.3.9.1";
     const ID_PREFIX = "madrang-plugin";
     console.log('%s SurpriseMe! Version: %s', ID_PREFIX, VERSION);
 
